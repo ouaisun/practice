be700b4b331caa2739c19f62e11f40c94066b7d2
jiangqingtian <jiangqingtian@didichuxing.com>
2018-04-21 13:19:39 +0800

    init

---
+++ src/main/java/io/moonnight/leetcode/Array/ArraySummaryRanges.java
@@ -0,0 +1,46 @@
+package io.moonnight.leetcode.Array;// Given a sorted integer array without duplicates, return the summary of its ranges.
+
+// For example, given [0,1,2,4,5,7], return ["0->2","4->5","7"].
+
+public class ArraySummaryRanges {
+
+    public List<String> summaryRanges(int[] nums) {
+        
+        List<String> result = new ArrayList();
+        
+        if(nums.length == 1) {
+            
+            result.add(nums[0] + "");
+            return result;
+            
+        }
+        
+        for(int i = 0; i < nums.length; i++) {
+            
+            int current = nums[i];
+            
+            while(i + 1 < nums.length && (nums[i + 1] - nums[i] == 1)) {
+                
+                i++;
+                
+            }
+            
+            if(current != nums[i]) {
+                
+                result.add(current + "->" + nums[i]);
+                
+            }
+            
+            else {
+                
+                result.add(current + "");
+                
+            }
+            
+        }
+        
+        return result;
+        
+    }
+
+}
---
+++ src/main/java/io/moonnight/leetcode/String/StringLongestPalindrome.java
@@ -0,0 +1,32 @@
+package io.moonnight.leetcode.String;
+
+public class StringLongestPalindrome {
+
+    public int longestPalindrome(String s) {
+        
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        
+        int count = 0;
+        
+        for(int i = 0; i < s.length(); i++) {
+            
+            if(!map.containsKey(s.charAt(i))) {
+                
+                map.put(s.charAt(i), (int)(s.charAt(i)));
+                
+            }
+            
+            else {
+                
+                map.remove(s.charAt(i));
+                count++;
+                
+            }
+            
+        }
+        
+        return map.isEmpty() ? count * 2 : count * 2 + 1;
+        
+    }
+    
+}
